45d28c81cd4281d529a0694c855be7e79bfafdba
lucas-alencar <lucasalencar.rd@gmail.com>
2023-01-09 09:19:58 -0300

    adding get in views

--- appCursos/views.py
+++ appCursos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+class AvaliacaoAPIView(APIView):
+    def get(self, request):
+        avaliacoes = Avaliacao.object.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
